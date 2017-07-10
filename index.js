fce294bd0d1eea88c358b568e6a495ebd222b35b
masad-frost <farismasad@gmail.com>
2017-07-11 01:30:07 +0300

    Add repl.it iframe

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(chrome.extension.getURL("index.html"))
+var params = new URL(document.location).searchParams;
+var packageName = params.get('packageName');
+
+// repl.it
+var requireCode = 'const ' + packageName + " = require('" + packageName + "');";
+var encodedRequireCode = encodeURIComponent(requireCode);
+var replitIframeUrl =
+  'https://repl.it/languages/babel?lite=true&code=' + encodedRequireCode;
+var replitIframe = document.createElement('iframe');
+replitIframe.style.width = '100%';
+replitIframe.style.height = '100%';
+replitIframe.src = replitIframeUrl;
+replitIframe.name = 'replit';
+var replitContainer = document.getElementById('replit');
+replitContainer.appendChild(replitIframe);
